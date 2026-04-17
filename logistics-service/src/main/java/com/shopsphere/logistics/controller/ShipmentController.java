a4272786cd30ac9cfa0c293af8f0a261b32a8282
Ankit Kumar <ankit.anjul@gmail.com>
2026-04-17 15:37:42 +0530

    FINAL NO REDIS

--- logistics-service/src/main/java/com/shopsphere/logistics/controller/ShipmentController.java
+++ logistics-service/src/main/java/com/shopsphere/logistics/controller/ShipmentController.java
@@ -2,6 +2,7 @@ package com.shopsphere.logistics.controller;
 
 import com.shopsphere.logistics.entity.Shipment;
 import com.shopsphere.logistics.service.ShipmentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/shipments")
+@Slf4j
 public class ShipmentController {
     private final ShipmentService shipmentService;
 
@@ -18,9 +20,11 @@ public class ShipmentController {
 
     @GetMapping
     public ResponseEntity<List<Shipment>> getAllShipments() {
+        log.info("Fetching all shipments");
         List<Shipment> shipments = shipmentService.getAllShipments();
 
         if (shipments.isEmpty()) {
+            log.info("No shipments found");
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(shipments);
@@ -28,21 +32,27 @@ public class ShipmentController {
 
     @PostMapping("/createShipment/{orderId}")
     public ResponseEntity<Shipment> createShipment(@PathVariable String orderId) {
+        log.info("Received request to create shipment for Order ID: {}", orderId);
         Shipment shipment = shipmentService.createShipment(orderId);
+        log.info("Shipment created successfully for Order ID: {}", orderId);
         return ResponseEntity.status(HttpStatus.CREATED).body(shipment);
     }
 
     @GetMapping("/order/{orderId}")
     public ResponseEntity<Shipment> getByOrderId(@PathVariable String orderId) {
+        log.info("Fetching shipment details for Order ID: {}", orderId);
         Shipment shipment = shipmentService.getShipmentByOrderId(orderId);
         return ResponseEntity.ok(shipment);
     }
+
     @PatchMapping("/order/{orderId}/{status}")
     public ResponseEntity<Shipment> updateStatusByOrderId(
             @PathVariable String orderId,
             @PathVariable String status) {
-
+        
+        log.info("Updating shipment status for Order ID: {} to {}", orderId, status);
         Shipment shipment = shipmentService.updateShipmentStatusByOrderId(orderId, status);
+        log.info("Shipment status updated successfully for Order ID: {}", orderId);
         return ResponseEntity.ok(shipment);
     }
 }
