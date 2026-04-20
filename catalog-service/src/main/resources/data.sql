5c66d0568180157addaadcfff4827d113554ca02
Ankit Kumar <ankit.anjul@gmail.com>
2026-04-20 11:27:22 +0530

    update

--- catalog-service/src/main/resources/data.sql
+++ catalog-service/src/main/resources/data.sql
@@ -18,8 +18,8 @@ INSERT IGNORE INTO custom_option (id, type, value, price_adjustment) VALUES (10,
 INSERT IGNORE INTO custom_option (id, type, value, price_adjustment) VALUES (11, 'Service', 'Extended 3-Year Warranty', 1500.0);
 
 
-INSERT IGNORE INTO product (product_id, name, base_price, preview_image) VALUES (1, 'Premium Hoodie', 1500.0, 'hoodie_thumb.jpg');
-INSERT IGNORE INTO product (product_id, name, base_price, preview_image) VALUES (2, 'Luxury Wallet', 3000.0, 'wallet_thumb.jpg');
+INSERT IGNORE INTO product (product_id, name, base_price, preview_image, is_active) VALUES (1, 'Premium Hoodie', 1500.0, 'hoodie_thumb.jpg', 1);
+INSERT IGNORE INTO product (product_id, name, base_price, preview_image, is_active) VALUES (2, 'Luxury Wallet', 3000.0, 'wallet_thumb.jpg', 1 );
 
 
 INSERT IGNORE INTO product_selected_options (product_id, option_id) VALUES (1, 2);
